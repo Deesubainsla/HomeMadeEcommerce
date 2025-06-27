318417158fd78e1e52e0c6aee00720fb41d6bbbe
Dinesh Kumar <dinesh.kumar@rdsolutions.in>
2025-06-27 17:21:21 +0530

    resolve scroll bottom issue

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Footer from './components/Footer'
 import Contact from './components/Contact'
 import Productdetails from './components/Productdetails'
 
+import ScrollToTop from './components/Scrolltotop'
+
 //for router
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
@@ -15,7 +17,7 @@ function App() {
 
   return (
     <BrowserRouter>
-
+      <ScrollToTop />
       <div className='min-h-screen max-w-screen'>
         {/* navbar */}
         <div className='sticky top-0 w-full z-1 shadow-sm'><Navbar /></div>
